48ec7af13f50efa7496f76dd6a0abcdc709b6a1d
Vikhil Parshotam <vikhilp98@gmail.com>
2021-04-30 14:35:16 +0100

    conditionals and colletions

---
+++ QACommunityTasks/collections.py
@@ -0,0 +1,5 @@
+books = {"Handmaiden's Tale":"Margaret Atwood",
+ "The Hobbit":"J.R.R. Tolkien",
+ "Charlie and the Chocolate Factory":"Roald Dahl"}
+
+print (books["The Hobbit"])
---
+++ QACommunityTasks/mark.py
@@ -0,0 +1,18 @@
+mark = int(input("What is your Mark?"))
+
+if mark >=85:
+    print("Distinction")
+elif mark >=65:
+    print("Pass")
+else:
+    print("Fail")
+
+# devs_money = 100
+# dev_can_play_smash = False
+
+# if devs_money > 10 and dev_can_play_smash:
+#     print("Dev enters a smash tornament!")
+# elif devs_money < 1 and dev_can_play_smash:
+#     print("Dev is too poor to enter")
+# else:
+#     print("Dev just can't play smash")
